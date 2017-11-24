03ff9d10a0d44c4b53bed34241e15629cc4f0edd
DeltaPos <Paz.Luis@hotmail.es>
2017-11-24 09:15:08 -0500

    n

--- application/views/ReporteGP.php
+++ application/views/ReporteGP.php
@@ -1,5 +1,26 @@
-<?php
-foreach ($provincias as $key => $value) {
-    echo $value->cedula_Estudiante;
-}
-
+<?php ?>
+<form name="form1" method="POST" action="<?php echo base_url(); ?>ReportesGP/Ado" >
+    
+    Notas X Materias
+    curso
+    
+    <select id="op" name="curso">
+        <option value="1">primero a</option>
+        <option value="2">primero b</option>
+        <option value="3">primero c</option>
+    </select>
+    materia
+    <select id="op" name="materia">
+        <option value="1">w</option>
+        <option value="2">Mate</option>
+        <option value="3">Historia</option>
+        <option value="5">primero c</option>
+        <option value="6">primero c</option>
+        <option value="7">primero c</option>
+        <option value="8">primero c</option>
+        <option value="9">primero c</option>
+        <option value="10">primero c</option>
+        <option value="11">primero c</option>
+    </select>
+    <input type="submit" value="Enviar" name="Enviar">
+</form>
